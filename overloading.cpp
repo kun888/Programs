e529678c21bfc024e348b68ce582e069c2576bb9
kunal chawla <kunalchawla2016@gmail.com>
2017-10-26 10:59:21 +0530

    Add files via upload

---
+++ overloading.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class com
+{
+public:
+int c1,c2;
+void get()
+{
+cin>>c1>>c2;
+}
+void disp()
+{
+cout<<c1<<c2;
+}
+};
+void com +(int obj)
+{
+com temp,c3;
+temp.c3=c1+c2.obj2;
+return temp;
+}
+void com -(int obj)
+{
+complex temp,c3;
+temp.c3=c1-c2.obj;
+return temp;
+}
+int main()
+{
+com obj1;
+obj1.set();
+obj1.disp();
+}
